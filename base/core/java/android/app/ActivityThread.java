ea9436ba95295e37577ae7f2d9c327f00ef893ed
wangheng <wangheng@mobimagic.com>
2018-01-24 15:32:28 +0800

    简单注释测试

--- base/core/java/android/app/ActivityThread.java
+++ base/core/java/android/app/ActivityThread.java
@@ -5067,6 +5067,9 @@ public final class ActivityThread {
         }
     }
 
+    /**
+     * Java层APP入口函数
+     */
     public static void main(String[] args) {
         SamplingProfilerIntegration.start();
 
